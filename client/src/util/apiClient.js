b7baedbe6097adfcbcc5bbe75cb38679954b5268
Marcus Ek <marcusekon@gmail.com>
2019-04-28 12:00:56 +0200

    Update apiClient.js

--- client/src/util/apiClient.js
+++ client/src/util/apiClient.js
@@ -5,7 +5,7 @@ import { HttpLink } from 'apollo-link-http'
 const cache = new InMemoryCache()
 
 const link = new HttpLink({
-	uri: 'http://localhost:3003/'
+	uri: 'http://localhost:3003/api'
 })
 
 export const apiClient = new ApolloClient({
