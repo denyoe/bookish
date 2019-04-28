6250045e0a9f4013ded2e648638834729a7df06e
Marcus Ek <marcusekon@gmail.com>
2019-04-29 00:52:45 +0200

    Build

---
+++ client/build/precache-manifest.29db9efba6bc728c46694b81e2d582d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f401f2e7fc860646275",
+    "url": "/static/css/main.2e427e79.chunk.css"
+  },
+  {
+    "revision": "9f401f2e7fc860646275",
+    "url": "/static/js/main.9753739f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b108f348316e87558859",
+    "url": "/static/js/2.d75e72b6.chunk.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "4c95cf0e269695cf4bf29f7a8d69c962",
+    "url": "/index.html"
+  }
+];
