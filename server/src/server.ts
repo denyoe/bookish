c6027e8ab67d8934f1504fe4514a7ca054be742a
Marcus Ek <marcusekon@gmail.com>
2019-04-28 11:44:23 +0200

    Cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,6 @@ import app from './app'
 
 const PORT = process.env.PORT || 3000
 
-// models.sequelize.sync().then(() => {
-//     app.listen(PORT)
-//     .then('error', onError)
-//     .then('listening', onListening)
-// });
-
 app
 .listen(PORT)
 .then(({ url }) => console.log(`🚀 Server ready at ${url}`))
